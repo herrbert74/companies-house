fa7602aed69486db1d8b81489d019687751163c2
herrbert74 <herrbert74@gmail.com>
2019-10-15 14:56:42 +0100

    Update to AS Beta 1

--- navigation/src/main/java/com/babestudios/companyinfouk/navigation/features/SignInNavigator.kt
+++
@@ -1,8 +0,0 @@
-package com.openpayd.navigation.features
-
-import com.babestudios.companyinfouk.navigation.base.GlobalNavigator
-import com.babestudios.companyinfouk.navigation.base.Navigator
-
-interface SignInNavigator: GlobalNavigator, Navigator {
-
-}
