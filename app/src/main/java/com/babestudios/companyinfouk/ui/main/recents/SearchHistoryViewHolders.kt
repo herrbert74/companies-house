5f6d5a477920d08be0c71949b4fa3b1551141b11
Zsolt Bertalan <herrbert74@gmail.com>
2019-11-04 22:56:18 +0000

    Fix for renamed layouts

--- app/src/main/java/com/babestudios/companyinfouk/ui/main/recents/SearchHistoryViewHolders.kt
+++ app/src/main/java/com/babestudios/companyinfouk/ui/main/recents/SearchHistoryViewHolders.kt
@@ -2,20 +2,20 @@ package com.babestudios.companyinfouk.ui.main.recents
 
 import android.view.View
 import com.babestudios.base.mvp.list.BaseViewHolder
-import kotlinx.android.synthetic.main.row_recent_searches.view.*
-import kotlinx.android.synthetic.main.row_recent_searches_title.view.*
+import kotlinx.android.synthetic.main.row_subtitle.view.*
+import kotlinx.android.synthetic.main.row_two_lines.view.*
 
 class SearchHistoryViewHolder(itemView: View) : BaseViewHolder<AbstractSearchHistoryVisitable>(itemView) {
 	override fun bind(visitable: AbstractSearchHistoryVisitable) {
 		val searchHistoryItem = (visitable as SearchHistoryVisitable).searchHistoryItem
-		itemView.lblRecentSearchesCompanyName.text = searchHistoryItem.companyName
-		itemView.lblRecentSearchesCompanyNumber.text = searchHistoryItem.companyNumber
+		itemView.lblCommonTwoLinesTitle.text = searchHistoryItem.companyName
+		itemView.lblCommonTwoLinesText.text = searchHistoryItem.companyNumber
 	}
 }
 
 class SearchHistoryHeaderViewHolder(itemView: View) : BaseViewHolder<AbstractSearchHistoryVisitable>(itemView) {
 	override fun bind(visitable: AbstractSearchHistoryVisitable) {
 		val searchHistoryHeaderItem = (visitable as SearchHistoryHeaderVisitable).searchHistoryHeaderItem
-		itemView.lblRecentSearchesTitle.text = searchHistoryHeaderItem.title
+		itemView.lblCommonSubtitle.text = searchHistoryHeaderItem.title
 	}
 }
