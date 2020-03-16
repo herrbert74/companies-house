0b2fb9dbbcc4f73d2d506b052ff49b730f783532
Zsolt Bertalan <herrbert74@gmail.com>
2020-03-16 14:26:21 +0000

    Update libraries

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 
 dependencies {
 	/* Depend on the android gradle plugin, since we want to access it in our plugin */
-	implementation("com.android.tools.build:gradle:4.0.0-alpha07")
+	implementation("com.android.tools.build:gradle:4.0.0-beta02")
 
 	/* Depend on the kotlin plugin, since we want to access it in our plugin */
-	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
 
 	implementation(kotlin("android-extensions"))
 
