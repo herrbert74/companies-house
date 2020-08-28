71db2931705e5a8fd35f48f17ec8c5d3b4f48971
Zsolt Bertalan <herrbert74@gmail.com>
2020-08-28 21:15:28 +0100

    Ref #94 Replace repeating links with a custom view on company screen

---
+++ common/src/main/java/com/babestudios/companyinfouk/core/views/SingleLineView.kt
@@ -0,0 +1,38 @@
+package com.babestudios.companyinfouk.core.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue.COMPLEX_UNIT_PX
+import android.view.View
+import android.widget.LinearLayout
+import com.babestudios.companyinfouk.common.R
+import kotlinx.android.synthetic.main.view_single_line.view.*
+
+/**
+ * A single line view with an ImageView and a TextView aligned horizontally
+ *
+ * You have to add focusable and clickable if you want to make the whole view clickable
+ */
+
+class SingleLineView @JvmOverloads constructor(
+		context: Context,
+		attrs: AttributeSet? = null,
+		defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+	init {
+		View.inflate(context, R.layout.view_single_line, this)
+		obtainStyleAttributes(context, attrs)
+	}
+
+	private fun obtainStyleAttributes(context: Context, attrs: AttributeSet?) {
+		context.theme.obtainStyledAttributes(attrs, R.styleable.SingleLineView, 0, 0).apply {
+			ivSingleLine.setImageResource(getResourceId(R.styleable.SingleLineView_imageViewSrc, 0))
+			val contentDescription = getString(R.styleable.SingleLineView_imageViewContentDescription)
+			contentDescription?.let { ivSingleLine.contentDescription = it }
+			lblSingleLine.text = getString(R.styleable.SingleLineView_text)
+			val textSize = getDimensionPixelSize(R.styleable.SingleLineView_textSize, 0)
+			if (textSize > 0) lblSingleLine.setTextSize(COMPLEX_UNIT_PX, textSize.toFloat())
+		}.recycle()
+	}
+}
