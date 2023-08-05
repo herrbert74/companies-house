ced46897e774c95ef38781ab157c6cb7d0b8ee21
Zsolt Bertalan <herrbert74@gmail.com>
2023-08-05 17:03:48 +0100

    Add ios project

---
+++ iosApp/CompanyInfoUk/CompanyInfoUkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyInfoUkApp.swift
+//  CompanyInfoUk
+//
+//  Created by Zsolt Bertalan on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CompanyInfoUkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
