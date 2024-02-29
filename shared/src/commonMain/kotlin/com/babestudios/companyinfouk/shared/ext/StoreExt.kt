a7329c63d56d834563079a243c8a7274d35cb612
Zsolt Bertalan <>
2024-02-29 15:19:33 +0000

    Remove unused code from asValue extension

--- shared/src/commonMain/kotlin/com/babestudios/companyinfouk/shared/ext/StoreExt.kt
+++ shared/src/commonMain/kotlin/com/babestudios/companyinfouk/shared/ext/StoreExt.kt
@@ -2,22 +2,18 @@ package com.babestudios.companyinfouk.shared.ext
 
 import com.arkivanov.decompose.Cancellation
 import com.arkivanov.decompose.value.Value
-import com.arkivanov.mvikotlin.core.store.Store
-import com.arkivanov.mvikotlin.core.rx.Disposable
 import com.arkivanov.mvikotlin.core.rx.observer
+import com.arkivanov.mvikotlin.core.store.Store
 
 typealias ValueObserver<T> = (T) -> Unit
 
 fun <T : Any> Store<*, T, *>.asValue(): Value<T> =
 	object : Value<T>() {
 
-		private var disposables = emptyMap<ValueObserver<T>, Disposable>()
-
 		override val value: T get() = state
 
 		override fun subscribe(observer: ValueObserver<T>): Cancellation {
 			val disposable = states(observer(onNext = observer))
-			this.disposables += observer to disposable
 			return Cancellation { disposable.dispose() }
 		}
 
