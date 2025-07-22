d953962659a954a7abbdc9919fb620d131a79ef6
Zsolt Bertalan <herrbert74@gmail.com>
2025-07-22 20:52:53 +0100

    Fix mocks, renaming went wrong!?

--- feature-main/src/androidTest/java/com/babestudios/companyinfouk/mock/mockCompaniesDocumentRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.babestudios.companyinfouk.mock
-
-import com.babestudios.companyinfouk.shared.domain.api.CompaniesDocumentRepository
-import dev.mokkery.mock
-
-fun mockCompaniesDocumentRepository(): CompaniesDocumentRepository {
-	return mock()
-}
